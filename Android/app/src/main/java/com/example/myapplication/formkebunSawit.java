82c0f61e42f97b36821a2481688c562e8589d385
Glenny Chudra <63528591+typonemma@users.noreply.github.com>
2023-06-29 20:47:11 +0700

    menu utama

--- Android/app/src/main/java/com/example/myapplication/formkebunSawit.java
+++ Android/app/src/main/java/com/example/myapplication/formkebunSawit.java
@@ -5,13 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class Login extends AppCompatActivity {
-Button lgnButton;
-
+public class formkebunSawit extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_formkebun_sawit);
     }
 }
